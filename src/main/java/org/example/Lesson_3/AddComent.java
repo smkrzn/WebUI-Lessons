f01714f9c3fd3d390718110c74b1ab55103e1b12
sergej <smk.forwork@gmail.com>
2022-11-25 10:58:38 +0300

    ДЗ-3

---
+++ src/main/java/org/example/Lesson_3/AddComent.java
@@ -0,0 +1,47 @@
+package org.example.Lesson_3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class AddComent {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        //options.addArguments("--headless");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://www.livejournal.com");
+
+        WebElement webInput = driver.findElement(By.cssSelector(".s-header-item__link--login"));
+        webInput.click();
+        WebElement webLogin = driver.findElement(By.id("user"));
+        webLogin.sendKeys("serg_test");
+        WebElement webPassword = driver.findElement(By.id("lj_loginwidget_password"));
+        webPassword.sendKeys("Gb131313");
+        WebElement webButtonIn = driver.findElement(By.cssSelector(".b-loginform-btn.b-loginform-btn--login.b-loginform-btn--auth.b-loginform-btn--center"));
+        webButtonIn.click();
+
+        WebElement userMenu = driver.findElement(By.cssSelector(".s-nav-item__name"));
+        userMenu.click();
+        WebElement addComent = driver.findElement(By.cssSelector(".actions-entryunit__text"));
+        addComent.click();
+        WebElement text = driver.findElement(By.xpath("//span[@data-slate-node='text']"));
+        text.sendKeys("test001");
+        WebElement submit = driver.findElement(By.xpath("//button[@name='submitpost']"));
+        submit.click();
+
+
+        //driver.quit();
+    }
+}
